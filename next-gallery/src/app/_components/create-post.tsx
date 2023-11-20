16bec9debfa5e27fadfe2f55ad05ab14a28daf86
harryg30 <gordenstein30@gmail.com>
2023-11-20 11:12:11 -0500

    new post form with preview

--- next-gallery/src/app/_components/create-post.tsx
+++ next-gallery/src/app/_components/create-post.tsx
@@ -1,13 +1,34 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { api } from "~/trpc/react";
 
 export function CreatePost() {
   const router = useRouter();
   const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState<File>();
+  const [preview, setPreview] = useState("");
+
+  function handleFile(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) {
+    const files = (e.target as HTMLInputElement).files;
+
+    if (files) {
+      setImage(files[0]);
+    }
+  }
+
+  useEffect(() => {
+    if (image != undefined) {
+      const objectUrl = URL.createObjectURL(image);
+
+      setPreview(objectUrl);
+    }
+  }, [image]);
 
   const createPost = api.post.create.useMutation({
     onSuccess: () => {
@@ -22,18 +43,63 @@ export function CreatePost() {
         e.preventDefault();
         createPost.mutate({ name });
       }}
-      className="flex flex-col gap-2"
+      className="container m-auto grid grid-cols-3 gap-3"
     >
+      <div className="col-span-1 flex flex-col justify-center">
+        <div className="flex flex-row justify-end">
+          <label>Upload an image</label>
+        </div>
+      </div>
+
+      <input
+        type="file"
+        onChange={(e) => handleFile(e)}
+        className="col-span-2"
+      />
+
+      {preview != "" ? (
+        <>
+          <div className="col-span-1"></div>
+          <div className="col-span-1">
+            <img src={preview} />
+          </div>
+          <div className="col-span-1"></div>
+        </>
+      ) : (
+        <></>
+      )}
+
+      <div className="col-span-1 flex flex-col justify-center">
+        <div className="flex flex-row justify-end">
+          <label>Title</label>
+        </div>
+      </div>
       <input
         type="text"
         placeholder="Title"
         value={name}
         onChange={(e) => setName(e.target.value)}
-        className="w-full rounded-full px-4 py-2 text-black"
+        className="col-span-1 w-full rounded-full px-4 py-2 text-black"
       />
+      <div className="col-span-1"></div>
+
+      <div className="col-span-1 flex flex-col justify-center">
+        <div className="flex flex-row justify-end">
+          <label>Description</label>
+        </div>
+      </div>
+      <input
+        type="text"
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        className="col-span-1 w-full rounded-full px-4 py-2 text-black"
+      />
+      <div className="col-span-1"></div>
+
       <button
         type="submit"
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
+        className="col-span-1 col-start-2 rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
         disabled={createPost.isLoading}
       >
         {createPost.isLoading ? "Submitting..." : "Submit"}
--- next-gallery/src/app/newPost/page.tsx
+++ next-gallery/src/app/newPost/page.tsx
@@ -1,12 +1,9 @@
-import Header from "../_components/Header";
+import { CreatePost } from "../_components/create-post";
 
 export default function NewPost() {
   return (
-    <div className="flex flex-col items-center justify-center pt-10">
-      <div className="flex flex-row gap-5">
-        <label>Upload an image</label>
-        <input type="file" id="image" />
-      </div>
+    <div className="flex flex-col items-center gap-5 pt-10">
+      <CreatePost />
     </div>
   );
 }
