e699972deb663159bfc8c370bfe8fa9813c19638
harryg30 <gordenstein30@gmail.com>
2023-11-20 17:06:14 -0500

    no async

--- next-gallery/src/app/art/page.tsx
+++ next-gallery/src/app/art/page.tsx
@@ -1,7 +1,7 @@
 import { api } from "~/trpc/server";
 
-export default async function Art() {
-  const post = await api.post.getLatest;
+export default function Art() {
+  const post = api.post.getLatest;
   console.log(post);
 
   return <p>stuff</p>;
