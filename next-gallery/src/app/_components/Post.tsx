fd2595c689fbf7546c229b37aff6ede39cd79bbe
harryg30 <gordenstein30@gmail.com>
2023-12-01 12:13:18 -0500

    post cards on page

--- next-gallery/src/app/_components/Post.tsx
+++ next-gallery/src/app/_components/Post.tsx
@@ -1,5 +1,3 @@
-import Picture from "./Picture";
-
 type post = {
   id: string;
   imageUrl: string;
@@ -18,10 +16,19 @@ type postProps = {
 export default function Post(props: postProps): JSX.Element {
   return (
     <>
-      <div>
-        <Picture imageUrl={props.post.imageUrl} />
-        <p>{props.post.description}</p>
-      </div>
+      <a
+        href="#"
+        className="mt-3 block  bg-gradient-to-b from-lightPink to-red "
+      >
+        <img
+          src={props.post.imageUrl}
+          className="h-64 w-full object-cover p-1 sm:h-80 lg:h-96"
+        />
+
+        <h3 className="text-center text-lg font-bold text-gray-900 sm:text-xl">
+          {props.post.description}
+        </h3>
+      </a>
     </>
   );
 }
