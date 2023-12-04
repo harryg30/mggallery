cc73a25823756199bf35393950977d7c204d72a8
harryg30 <gordenstein30@gmail.com>
2023-12-04 16:06:46 -0500

    hover over post css

--- next-gallery/src/app/_components/Post.tsx
+++ next-gallery/src/app/_components/Post.tsx
@@ -9,14 +9,14 @@ export default function Post(props: postProps): JSX.Element {
     <>
       <a
         href={"art/post/" + props.post.id}
-        className="mt-3 block  bg-gradient-to-b from-lightPink to-red"
+        className="mt-3 block  rounded bg-gradient-to-b from-lightPink to-red hover:shadow-lg "
       >
         <Image
           src={props.post.imageUrl}
           alt={""}
           width={900}
           height={900}
-          className="h-64 w-full object-cover p-1 sm:h-80 lg:h-96"
+          className="h-64 w-full rounded object-cover p-2 hover:p-1 sm:h-80 lg:h-96"
         />
 
         <h3 className="text-center text-lg font-bold text-gray-900 sm:text-xl">
